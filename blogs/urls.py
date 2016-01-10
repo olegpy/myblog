f336a8eebd6a970c0348a3db0bc6689083fe9a13
olegggg <olegkriklivets@gmail.com>
2016-01-10 20:01:04 +0200

    add some classes in Blog

--- blogs/urls.py
+++ blogs/urls.py
@@ -2,10 +2,15 @@ from django.conf.urls import url
 from blogs import views
 
 urlpatterns = [
-    # url(r'^$', views.post_list, name='post_list'),
     url(r'^$', views.PostListView.as_view(), name='post_list'),
-    url(r'^new/$', views.add_post, name='add_post'),
-    url(r'^(?P<pk>\d+)/edit/$', views.edit_post, name='edit_post'),
-    url(r'^(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
+    # url(r'^$', views.post_list, name='post_list'),
+    url(r'^new/$', views.PostCreateView.as_view(), name='add_post'),
+    # url(r'^new/$', views.add_post, name='add_post'),
+    url(r'^edit/(?P<pk>\d+)/$', views.PostUpdateView.as_view(), name='edit_post'),
+    # url(r'^(?P<pk>\d+)/edit/$', views.edit_post, name='edit_post'),
+    url(r'^(?P<pk>\d+)/$', views.PostDetailView.as_view(), name='post_detail'),
+    # url(r'^(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
+    url(r'^remove/(?P<pk>\d+)/$',
+                           views.PostDeleteView.as_view(), name='remove_post'),
 
 ]
--- blogs/views.py
+++ blogs/views.py
@@ -1,12 +1,12 @@
-from django.shortcuts import render
 from django.utils import timezone
-from django.shortcuts import redirect
 from django.contrib import messages
+from django.core.urlresolvers import reverse_lazy
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
 from .models import Post
-from .forms import PostModelForm
+
 
 class PostListView(ListView):
     model = Post
@@ -14,77 +14,110 @@ class PostListView(ListView):
     template_name = 'blogs/post_list.html'
     context_object_name = 'posts'
 
-    # def queryset(self):
-    #     qs = super(StudentListView, self).get_queryset()
-    #     # if course_id:
-    #     qs = qs.filter(published_date__lte=timezone.now())
-        # return qs
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blogs/post_detail.html'
+    context_object_name = 'post'
+
+
+class PostCreateView(CreateView):
+    model = Post
+    fields = ['title', 'text']
+    template_name = 'blogs/post_edit.html'
+    context_object_name = 'form'
+
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        self.object.author = self.request.user
+        self.object.published_date = timezone.now()
+        self.object.save()
+        messages.success(
+            self.request, 'Post %s has been add successfully.' % (self.object.title))
+        return super(PostCreateView, self).form_valid(form)
+
+    def get_success_url(self, **kwargs):
+        return reverse_lazy('post_detail', args=(self.object.pk,))
+
+
+class PostUpdateView(UpdateView):
+    model = Post
+    fields = ['title', 'text']
+    template_name = 'blogs/post_edit.html'
+    context_object_name = 'form'
+
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        self.object.author = self.request.user
+        self.object.published_date = timezone.now()
+        self.object.save()
+        messages.success(
+            self.request, 'Post %s has been successfully edit.' % (self.object.title))
+        return super(PostUpdateView, self).form_valid(form)
+
+    def get_success_url(self, **kwargs):
+        return reverse_lazy('post_detail', args=(self.object.pk,))
+
+
+class PostDeleteView(DeleteView):
+    model = Post
+    fields = ['title', 'text']
+    template_name = 'blogs/post_remove.html'
+    context_object_name = 'post'
+    success_url = reverse_lazy('post_list')
+
+    def get_context_data(self, **kwargs):
+        context = super(PostDeleteView, self).get_context_data(**kwargs)
+        context['title'] = "Post deletion"
+        return context
+
+    def delete(self, request, *args, **kwargs):
+        message = super(PostDeleteView, self).delete(
+            request, *args, **kwargs)
+        messages.success(self.request, 'Post %s has been deleted.' % (
+            self.object.title))
+        return message
+
 # def post_list(request):
 #     posts = Post.objects.filter(
 #         published_date__lte=timezone.now()).order_by('published_date')
 #     return render(request, 'blogs/post_list.html', {'posts': posts})
 
-# def list_view(request):
-#     reguest_course = request.GET
-#     if 'course_id' in reguest_course:
-#         list_students = Student.objects.filter(
-#             courses=reguest_course['course_id'])
+
+
+# def post_detail(request, pk):
+#     post = Post.objects.get(id=pk)
+#     return render(request, 'blogs/post_detail.html', {'post': post})
+
+
+# def add_post(request):
+#     if request.method == 'POST':
+#         form = PostModelForm(request.POST)
+#         if form.is_valid():
+#             post = form.save(commit=False)
+#             post.author = request.user
+#             post.published_date = timezone.now()
+#             post.save()
+#             messages.success(request, 'Post %s has been add successfully.' % (
+#                 post.title))
+#             return redirect('post_detail', post.id)
 #     else:
-#         list_students = Student.objects.all()
-# return render(request, 'students/list.html', {'list_students':
-# list_students})
+#         form = PostModelForm()
+#     return render(request, 'blogs/post_edit.html', {'form': form})
 
-def post_detail(request, pk):
-    post = Post.objects.get(id=pk)
-    return render(request, 'blogs/post_detail.html', {'post': post})
+# def edit_post(request, pk):
+#     post = Post.objects.get(id=pk)
+#     if request.method == 'POST':
+#         form = PostModelForm(request.POST, instance=post)
+#         if form .is_valid():
+#             post = form.save(commit=False)
+#             post.author = request.user
+#             post.published_date = timezone.now()
+#             post.save()
+#             messages.success(request, 'Post %s has been successfully edit.' % (
+#                 post.title))
+#             return redirect('post_detail', post.id)
 
-
-def add_post(request):
-    if request.method == 'POST':
-        form = PostModelForm(request.POST)
-        if form.is_valid():
-            post = form.save(commit=False)
-            post.author = request.user
-            post.published_date = timezone.now()
-            post.save()
-            messages.success(request, 'Post %s has been add successfully.' % (
-                post.title))
-            return redirect('post_detail', post.id)
-    else:
-        form = PostModelForm()
-    return render(request, 'blogs/post_edit.html', {'form': form})
-
-def edit_post(request, pk):
-    post = Post.objects.get(id=pk)
-    if request.method == 'POST':
-        form = PostModelForm(request.POST, instance=post)
-        if form .is_valid():
-            post = form.save(commit=False)
-            post.author = request.user
-            post.published_date = timezone.now()
-            post.save()
-            messages.success(request, 'Post %s has been successfully edit.' % (
-                post.title))
-            return redirect('post_detail', post.id)
-
-    else:
-        form = PostModelForm(instance=post)
-    return render(request, 'blogs/post_edit.html', {'form': form})
-
-
-#
-# def detail(request, pk):
-#     student = Student.objects.get(id=pk)
-#     return render(request, 'students/detail.html', {'student': student})
-
-
-
-# def detail(request, pk):
-#     coaches = Coach.objects.get(id=pk)
-#     context = {'coaches': coaches}
-
-#     coach_courses = Course.objects.filter(coach=pk)
-#     assistants_courses = Course.objects.filter(assistant=pk)
-#     context['coach_courses'] = coach_courses
-#     context['assistants_courses'] = assistants_courses
-#     return render(request, 'coaches/detail.html', context)
+#     else:
+#         form = PostModelForm(instance=post)
+#     return render(request, 'blogs/post_edit.html', {'form': form})
