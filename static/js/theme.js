26a3eb0e3a2059ff01278a30e3ce2f22f9587333
olegggg <olegkriklivets@gmail.com>
2016-01-27 21:10:55 +0200

    add some changes

--- static/js/theme.js
+++ static/js/theme.js
@@ -1,7 +1,11 @@
 //this code needed for identification for vk 
+// vk init
 VK.init({
-	apiId: 5238735, onlyWidgets: true
+	apiId: 5238735, 
+	onlyWidgets: true
 });
+
+// vk auth login
 VK.Auth.getLoginStatus(function(response) {
 	if (response.session) {
 		if (window.location.pathname == "/") {
@@ -11,11 +15,11 @@ VK.Auth.getLoginStatus(function(response) {
 });
 
 
-$(document).ready(function(){
-    $("#comments").on("click", ".reply", function(event){
-        event.preventDefault();
-        var form = $("#comment_post").clone(true);
-        form.find('.parent').val($(this).parent().parent().attr('id'));
-        $(this).parent().append(form);
-    });
-});
+// $(document).ready(function(){
+//     $("#comments").on("click", ".reply", function(event){
+//         event.preventDefault();
+//         var form = $("#comment_post").clone(true);
+//         form.find('.parent').val($(this).parent().parent().attr('id'));
+//         $(this).parent().append(form);
+//     });
+// });
