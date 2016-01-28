857652c8926ab63763020dedb329a43c96d3fcfb
olegggg <olegkriklivets@gmail.com>
2016-01-28 21:15:56 +0200

    add some settings to comment

--- blogs/urls.py
+++ blogs/urls.py
@@ -3,21 +3,24 @@ from blogs import views
 
 urlpatterns = [
     url(r'^$', views.PostListView.as_view(), name='post_list'),
-    # url(r'^$', views.post_list, name='post_list'),
-    url(r'^new/$', views.PostCreateView.as_view(), name='add_post'),
-    # url(r'^new/$', views.add_post, name='add_post'),
-    url(r'^edit/(?P<pk>\d+)/$',
-        views.PostUpdateView.as_view(), name='edit_post'),
-    # url(r'^(?P<pk>\d+)/edit/$', views.edit_post, name='edit_post'),
     url(r'^(?P<pk>\d+)/$',
         views.PostDetailView.as_view(), name='post_detail'),
-    # url(r'^(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
+
+    url(r'^new/$', views.PostCreateView.as_view(), name='add_post'),
+
+    url(r'^edit/(?P<pk>\d+)/$',
+        views.PostUpdateView.as_view(), name='edit_post'),
+
     url(r'^remove/(?P<pk>\d+)/$',
         views.PostDeleteView.as_view(), name='remove_post'),
 
-    url(r'^comment/(?P<pk>\d+)/$',
+    url(r'^add/(?P<pk>\d+)/comment/$',
         views.CommentCreateView.as_view(), name='add_comment_post'),
 
-    # url(r'^comment/(?P<pk>\d+)/$', views.add_comment_post, name='add_comment_post'),
+    url(r'^edit/(?P<pk>\d+)/comment/$',
+        views.CommentUpdateView.as_view(), name='edit_comment'),
+
+    url(r'^remove/(?P<pk>\d+)/comment/$',
+        views.CommentDeleteView.as_view(), name='remove_comment'),
 
 ]
--- blogs/views.py
+++ blogs/views.py
@@ -105,6 +105,52 @@ class CommentCreateView(CreateView):
         return self.object.get_url()
 
 
+class CommentUpdateView(UpdateView):
+    model = Comment
+    form_class = CommentModelForm
+    template_name = 'blogs/post_comment.html'
+    context_object_name = 'form'
+
+    def get_context_data(self, **kwargs):
+        context = super(CommentUpdateView, self).get_context_data(**kwargs)
+        context['page_title'] = "Comment update"
+        return context
+
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        messages.success(
+            self.request, 'Comment %s has been successfully edit.' % (self.object.post))
+        return super(CommentUpdateView, self).form_valid(form)
+
+    def get_success_url(self, **kwargs):
+        return reverse_lazy('post_detail', args=(self.object.pk,))
+
+
+class CommentDeleteView(DeleteView):
+    model = Comment
+    form_class = CommentModelForm
+    template_name = 'blogs/remove.html'
+    context_object_name = 'post'
+
+    def get_context_data(self, **kwargs):
+        context = super(CommentDeleteView, self).get_context_data(**kwargs)
+        context['page_title'] = "Comment deletion"
+        return context
+
+    def delete(self, request, *args, **kwargs):
+        message = super(CommentDeleteView, self).delete(
+            request, *args, **kwargs)
+        messages.success(self.request, 'Comment post %s has been deleted.' % (
+            self.object.post))
+        return message
+
+    
+    def get_success_url(self, **kwargs):
+        return reverse_lazy('edit_comment', args=(self.object.pk,))
+
+
+
+
 # class CommentCreateView(CreateView):
 #     model = Comment
 #     form_class = CommentModelForm
@@ -144,63 +190,64 @@ class CommentCreateView(CreateView):
 #         return reverse_lazy('post_detail', args=(self.object.pk, ))
 
 
-def add_comment_post(request, pk):
-    post=get_object_or_404(Post, pk = pk)
-    if request.method == "POST":
-        form=CommentModelForm(request.POST)
-        if form.is_valid():
-            comment=form.save(commit = False)
-            print comment
-            print post
-            comment.post=post
-            comment.save()
-            return redirect('post_detail', pk = post.pk)
-    else:
-        form=CommentModelForm()
-    return render(request, 'blogs/post_comment.html', {'form': form})
+# def add_comment_post(request, pk):
+#     post=get_object_or_404(Post, pk = pk)
+#     if request.method == "POST":
+#         form=CommentModelForm(request.POST)
+#         if form.is_valid():
+#             comment=form.save(commit = False)
+#             print comment
+#             print post
+#             comment.post=post
+#             comment.save()
+#             return redirect('post_detail', pk = post.pk)
+#     else:
+#         form=CommentModelForm()
+#     return render(request, 'blogs/post_comment.html', {'form': form})
 
 
 class PostUpdateView(LoginRequiredMixin, UpdateView):
-    model=Post
-    form_class=PostModelForm
-    template_name='blogs/post_edit.html'
-    context_object_name='form'
+    model = Post
+    form_class = PostModelForm
+    template_name = 'blogs/post_edit.html'
+    context_object_name = 'form'
 
     def get_context_data(self, **kwargs):
-        context=super(PostUpdateView, self).get_context_data(**kwargs)
-        context['page_title']="Post update"
+        context = super(PostUpdateView, self).get_context_data(**kwargs)
+        context['page_title'] = "Post update"
         return context
 
     def form_valid(self, form):
-        self.object=form.save(commit = False)
-        self.object.author=self.request.user
-        self.object.published_date=timezone.now()
+        self.object = form.save(commit=False)
+        self.object.author = self.request.user
+        self.object.published_date = timezone.now()
         self.object.save()
         messages.success(
             self.request, 'Post %s has been successfully edit.' % (self.object.title))
         return super(PostUpdateView, self).form_valid(form)
 
     def get_success_url(self, **kwargs):
-        return reverse_lazy('post_detail', args = (self.object.pk,))
+        return reverse_lazy('post_detail', args=(self.object.pk,))
 
 
 class PostDeleteView(DeleteView):
-    model=Post
-    form_class=PostModelForm
-    # fields = ['title', 'text']
-    template_name='blogs/post_remove.html'
-    context_object_name='post'
+    model = Post
+    form_class = PostModelForm
+    template_name = 'blogs/remove.html'
+    context_object_name = 'post'
+    success_url = reverse_lazy('post_list')
+
+
 
     def get_context_data(self, **kwargs):
-        context=super(PostDeleteView, self).get_context_data(**kwargs)
-        context['page_title']="Post deletion"
+        context = super(PostDeleteView, self).get_context_data(**kwargs)
+        context['page_title'] = "Post deletion"
         return context
 
     def delete(self, request, *args, **kwargs):
-        message=super(PostDeleteView, self).delete(
+        message = super(PostDeleteView, self).delete(
             request, *args, **kwargs)
-        messages.success(self.request, 'Post %s has been deleted.' % (
-            self.object.title))
+        messages.success(self.request, 'Comment has been deleted.')
         return message
 
 
