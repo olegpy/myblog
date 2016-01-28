65f28db5f1c3e748943ee5480f9ffaa1104818e8
olegggg <olegkriklivets@gmail.com>
2016-01-28 22:44:12 +0200

    add some settings to comment

--- blogs/views.py
+++ blogs/views.py
@@ -25,11 +25,6 @@ class PostListView(ListView):
     template_name = 'blogs/post_list.html'
     context_object_name = 'posts'
 
-    # def get_queryset(self):
-    #     """Return the last five published questions."""
-    #     queryset = super(PostListView, self).get_queryset(
-    #     ).order_by('created_date')
-
     def get_context_data(self, **kwargs):
         context = super(PostListView, self).get_context_data(**kwargs)
         context['page_title'] = "Post list"
@@ -69,50 +64,6 @@ class PostCreateView(LoginRequiredMixin, CreateView):
             self.request, 'Post %s has been add successfully.' % (self.object.title))
         return super(PostCreateView, self).form_valid(form)
 
-    # def get_success_url(self, **kwargs):
-    #     return reverse_lazy('post_list', args=(self.object.pk,))
-
-
-# class CommentCreateView(CreateView):
-#     model = Comment
-#     form_class = CommentModelForm
-#     template_name = 'blogs/post_comment.html'
-#     context_object_name = 'form'
-
-#     def get_context_data(self, **kwargs):
-#         context = super(CommentCreateView, self).get_context_data(**kwargs)
-#         context['page_title'] = 'Add comments'
-#         return context
-
-#     def form_valid(self, form):
-#         # link = get_object_or_404(Comment, pk=self.post.pk)
-#         # print link
-
-#         comment = form.save(commit=False)
-
-#         name = form.cleaned_data
-
-#         print self.request.post.id
-#         # print self.request.post
-#         # comment.post = self.post
-#         comment.save()
-
-#         # comment = form.save(commit=False)
-#         # comment.author = self.request.post
-#         # # self.object.published_date = timezone.now()
-#         # comment.save()
-
-#         data = form.cleaned_data
-#         messages.success(
-#             self.request, 'Comment %s has been successfully.' % (data[
-#                                                                  'author']))
-#         return super(CommentCreateView, self).form_valid(form)
-
-#     def get_success_url(self, **kwargs):
-#         return reverse_lazy('post_detail', args=(self.object.pk, ))
-
-
-#
 
 class PostUpdateView(LoginRequiredMixin, UpdateView):
     model = Post
@@ -155,30 +106,6 @@ class PostDeleteView(DeleteView):
             request, *args, **kwargs)
         messages.success(self.request, 'Comment has been deleted.')
         return message
-
-
-# class PostCreateView(LoginRequiredMixin, CreateView):
-#     model = Post
-#     form_class = PostModelForm
-#     template_name = 'blogs/post_edit.html'
-#     context_object_name = 'form'
-
-#     def get_context_data(self, **kwargs):
-#         context = super(PostCreateView, self).get_context_data(**kwargs)
-#         context['page_title'] = "Post create"
-#         return context
-
-#     def form_valid(self, form):
-#         self.object = form.save(commit=False)
-#         self.object.author = self.request.user
-#         self.object.published_date = timezone.now()
-#         self.object.save()
-#         messages.success(
-#             self.request, 'Post %s has been add successfully.' % (self.object.title))
-#         return super(PostCreateView, self).form_valid(form)
-
-#     def get_success_url(self, **kwargs):
-#         return reverse_lazy('post_list', args=(self.object.pk,))
 
 
 def add_comment_post(request, pk):
